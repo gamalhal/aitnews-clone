cc580452b855df83830d8adce488635b0ef4c848
Gamal Hal <gamalhal.chi@gmail.com>
2025-09-02 18:19:04 +0300

    push project  clone  aitnews.com

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // للبحث في ملف HTML الرئيسي
+    "./src/**/*.{js,ts,jsx,tsx}", // للبحث في جميع ملفات رياكت داخل مجلد src
+  ],
+  theme: {
+    extend: {
+      // هنا يمكنك إضافة ألوان أو خطوط مخصصة في المستقبل
+    },
+  },
+  plugins: [],
+}
